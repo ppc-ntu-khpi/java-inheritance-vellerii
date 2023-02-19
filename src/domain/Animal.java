281a74ba435bf087e9c503605f31071663a2eb43
Valeriia Krychevska <90756841+vellerii@users.noreply.github.com>
2023-02-19 20:44:42 +0200

    Added classes

--- src/domain/Animal.java
+++ src/domain/Animal.java
@@ -3,26 +3,31 @@ package domain;
 public class Animal {
 
     protected String name;
-
-    protected int weight;
+    
+    protected Integer weight;
+    
+  
 
     public Animal() {
-        name="generic animal";
-        weight=25;
+        name = "generic animal";
+        weight = 15;
     }
-
+    
     public void eat() {
         System.out.println("Animal eating...");
     }
-
+    
     public void speak() {
         System.out.println("Animal speaking...");
     }
+    
+    public void sleep() {
+        System.out.println("Animal sleeping...");
+    }
+    
 
     @Override
     public String toString() {
-        return "My animal:" + "\nName:\t" + name + "\nWeight:\t" + weight+" kg";
+        return "My animal:" + "\nName:\t" + name + "\nWeight:\t" + weight + " kg";
     }
-
- 
 }
